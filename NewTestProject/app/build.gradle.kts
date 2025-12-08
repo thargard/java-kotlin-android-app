7f74a93ea1d79a156cdecaee11b1ed495bd3d38d
Gleb <gleb.margolin.1301@gmail.com>
2025-12-08 15:14:51 +0300

    refactor to single-activity structure android app

--- NewTestProject/app/build.gradle.kts
+++ NewTestProject/app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+
+    implementation ("com.google.android.material:material:1.12.0")
     implementation ("androidx.compose.ui:ui:1.5.0")
     implementation ("androidx.compose.material3:material3:1.2.1")
     implementation ("androidx.activity:activity-compose:1.7.2")
@@ -70,4 +72,11 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
     implementation ("com.google.code.gson:gson:2.11.0")
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+
+    implementation("androidx.compose.material:material-icons-core:1.6.0")
+    implementation("androidx.compose.material:material-icons-extended:1.6.0")
+    implementation("androidx.navigation:navigation-compose:2.7.6")
 }
